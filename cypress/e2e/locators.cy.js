094a39f8bc4190069d00155ed02a3e25410fbbed
chrs0129 <chrs0129@gmail.com>
2023-05-11 16:41:00 -0400

    add: new locators stratagie

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -9,18 +9,24 @@ describe("Find or Get Elements by Using Differen Locators", () => {
   beforeEach(() => {
     // run before each test case, beforeMethod in TestNG
     cy.clearCookies();
-    cy.visit('/login');
+    cy.visit("/login");
   });
- 
-  
+
   it("Check different locators strategies", () => {
-    cy.get("input[name='username']").type('CydeoStudent');
+    cy.get("input[name='username']").type("CydeoStudent");
     cy.get("[type='text']").clear();
-    cy.get("input").each((item, index, list)=> {
-        expect(list).to.have.length(2);
-        expect(item).to.have.attr('type');
-    })
+    cy.get("input").each((item, index, list) => {
+      expect(list).to.have.length(2);
+      expect(item).to.have.attr("type");
+         });
+          // by attribute name
+      cy.get("[type]");
+      // by className
+      cy.get(".btn.btn-primary");
+      // by id
+      cy.get("#wooden_spoon");
+      // by text
+      cy.get('button').should('contain', 'Login').click();
   })
 
-  
 })
