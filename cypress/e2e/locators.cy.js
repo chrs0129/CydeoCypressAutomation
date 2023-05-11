e6fc23ed949af566f783b02afdb72610b8d6700f
chrs0129 <chrs0129@gmail.com>
2023-05-11 15:47:11 -0400

    add: more tests2

---
+++ cypress/e2e/locators.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+
+
+describe("Find or Get Elements by Using Differen Locators", () => {
+  before(() => {
+    // runs once before all test cases in this describe block, like beforeClass in TestNG
+  });
+  beforeEach(() => {
+    // run before each test case, beforeMethod in TestNG
+    cy.clearCookies();
+    cy.visit('/login');
+  });
+ 
+  
+  it("Check different locators strategies", () => {
+    cy.get("input[name='username']").type('CydeoStudent');
+    cy.get("[type='text']").clear();
+    cy.get("input").each((item, index, list)=> {
+        expect(list).to.have.length(2);
+        expect(item).to.have.attr('type');
+    })
+  })
+
+  
+})
