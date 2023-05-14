c31b7a881aec0017da6a923a78c0f7b6704eb7d6
chrs0129 <chrs0129@gmail.com>
2023-05-14 12:32:47 -0400

    add:third week

---
+++ cypress/e2e/fileUpload.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+const cypress = require("cypress");
+
+describe("Context: My First Tests", () => {
+    beforeEach('Navigate to upload page', () => {
+        cy.clearCookies();
+        cy.visit('/upload');
+      });
+     
+      it('Check Upload Action', () => {
+          // locator for choose file button
+        cy.get('input#file-upload').attachFile('pic.png');
+         // click on uıpload button
+         cy.get('#file-submit').click();
+         // assert that path message is displayed
+         cy.get('#uploaded-files').then(() => {
+              cy.contains('pic.png').should('be.visible');
+         });
+      });
+
+  
+})
--- cypress/e2e/inputs.cy.js
+++ cypress/e2e/inputs.cy.js
@@ -1,12 +1,9 @@
 /// <reference types="cypress" />
 
-const cypress = require("cypress");
-
-describe("Input form  Tests", () => {
-  beforeEach("Navigat ot Registration Page", () => {
-    // run before each test case, beforeMethod in TestNG
+describe('Input Forms Tests', () => {
+  beforeEach('Navigate to registration page', () => {
     cy.clearCookies();
-    cy.visit("/registration_form");
+    cy.visit('/registration_form');
   });
 
   it.skip('Check different input box fields and verify', () => {
@@ -28,52 +25,53 @@ describe("Input form  Tests", () => {
   });
   it.skip('Check different radio button actions', () => {
     cy.get('.radio')
-    .find('[type=radio]')
-    .then((radio => {
-     // get all radio buttons, select the first one and verify that it is checked
-     cy.wrap(radio).first().check().should('be.checked'); // cypres works in a chainable functions structure
-     /**
-      * radio : is Jquery element, cy.wrap(radio) : turns it into Cypress Object so that I can use cypress functions
-      * first() : selects first element
-      * check() : checks it out
-      * should(): verifes whatever I provide as parameter 'be.checked'
-      */
-     // get all radio buttons, select the second one and verify that it is checked and confirmation label is visible
-     cy.wrap(radio).eq(1).check().should('be.checked');
-     cy.get('[data-bv-icon-for="gender"]').should('be.visible'); // common function used in tests
-     // Third radio button is NOT checked
-     cy.wrap(radio).eq(2).should('not.be.checked');
-    })) 
-})
-it.skip('Check different checkbox actions',() => {
+      .find('[type=radio]')
+      .then((radio) => {
+        // get all radio buttons, select the first one and verify that it is checked
+        cy.wrap(radio).first().check().should('be.checked'); // cypres works in a chainable functions structure
+        /**
+         * radio : is Jquery element, cy.wrap(radio) : turns it into Cypress Object so that I can use cypress functions
+         * first() : selects first element
+         * check() : checks it out
+         * should(): verifes whatever I provide as parameter 'be.checked'
+         */
+        // get all radio buttons, select the second one and verify that it is checked and confirmation label is visible
+        cy.wrap(radio).eq(1).check().should('be.checked');
+        cy.get('[data-bv-icon-for="gender"]').should('be.visible'); // common function used in tests
+        // Third radio button is NOT checked
+        cy.wrap(radio).eq(2).should('not.be.checked');
+      });
+  });
+  it.skip('Check different checkbox actions', () => {
     // get all chechboxes, select JAVA and verify
     cy.get('[type="checkbox"]').then((checkbox) => {
-        cy.wrap(checkbox).eq(1).check().should('be.checked');
-        // uncheck JAVA
-        cy.wrap(checkbox).eq(1).uncheck().should('not.be.checked');
-        cy.wrap(checkbox).eq(2).should('have.value', 'javascript')
-        .check().should('be.checked');
-    })
-  })
-  it('Check selection of single choice from select dropdown options', ()=>{
-    cy.get('select[name="job_title"]').select("SDET");
-    cy.get('select[name="job_title"]').select("SDET").contains("SDET");
-
-  })
-  it('Check selection of all dropdown options', ()=>{
-    cy.fixture("deparments").then((departments) =>{
-        cy.get('select[name="department"] > option').each((option, index)=>{
-            const optionText= option.text();
+      cy.wrap(checkbox).eq(1).check().should('be.checked');
+      // uncheck JAVA
+      cy.wrap(checkbox).eq(1).uncheck().should('not.be.checked');
+      // verify third one has a value Javascript and then check and verify
+      cy.wrap(checkbox).eq(2).should('have.value', 'javascript').check().should('be.checked');
+    });
+  });
+  it.skip('Check selection of a single choice from a select dropdown', () => {
+    // select one element
+    cy.get('select[name="job_title"]').select('SDET');
+    // assert that dropdown has correct text after selecting
+    cy.get('select[name="job_title"]').contains('SDET');
+  });
+  it('Check selection of all select dropdowns options', () => {
+    // we will provide our test data through fixtures folder as JSON object, then use that data to verify select values
+    cy.fixture('departments').then((departments) => {
+        // Get all options in the menu, iterate through these options one by one
+        cy.get('select[name="department"] > option').each((option, index) => {
+            // get each option text
+            const optionText = option.text();
            // cy.log(optionText);
-          //  cy.log(index);
-          //  cy.log(departments[index]);
-          cy.get('select[name="department"]').select(optionText)
-          .should('have.value', option.val())
-          // .should('have.text', departments[index])
-          .contains(departments[index]);
+           // cy.log(index);
+           // cy.log(departments[index]);
+           cy.get('select[name="department"]').select(optionText)
+           .should('have.value',option.val())
+           .contains(departments[index]);
         })
     })
-
-  })
+  });
 });
-
